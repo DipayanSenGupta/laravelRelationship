53a3b76f44b409c1b8c8877dd84c917f3126d08c
dip.himu@gmail.com <dip.himu@gmail.com>
2019-05-03 11:53:27 +0600

    one-to-one & its inverse

---
+++ database/seeds/AccountsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Account;
+use Illuminate\Database\Seeder;
+
+class AccountsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('accounts')->truncate();
+    	$faker = \Faker\Factory::create();
+    	foreach(range(1,50) as $index)
+    	{
+    		Account::create([
+    			'user_id' => $index,
+    			 'account_number'=> rand(100000000,999999999),
+    		]);
+    	}
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('users')->truncate();
+    	$faker = \Faker\Factory::create();
+    	foreach(range(1,50) as $index)
+    	{
+    		User::create([
+    			'name' => $faker->userName,
+    			'email' => $faker->email,
+    			'password' => bcrypt('secret'),
+    			// $table->bigIncrements('id');
+            // $table->string('name');
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password'); 
+    		]);
+    	}
+    }
+}
